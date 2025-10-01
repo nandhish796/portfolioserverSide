d9afc3bb4e711986d91749c261c90f98009e585d
nandhish796 <nandhu17022006@gmail.com>
2025-10-01 19:36:03 +0530

    first commit

---
+++ email.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/send", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  if (!name || !email || !message) {
+    return res.status(400).json({ msg: "Please fill all fields" });
+  }
+
+  try {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user:"portfolio1702@gmail.com",
+        pass:"szbz oysk dgvd lurx",
+      },
+    });
+
+    let mailOptions = {
+      from: email,
+      to: "portfolio1702@gmail.com",
+      subject: `Contact Form Submission from ${name}`,
+      text: message,
+      replyTo: email,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ msg: "Email sent successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Failed to send email" });
+  }
+});
+
+const PORT =5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
